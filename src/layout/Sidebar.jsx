e5376ca8523b3898fc19295f6ba2314c0912c0d8
OlegColeg <ooale47@gmail.com>
2025-05-01 22:23:09 +0300

    feat: enhance button styles in Dashboard and update Sidebar button color for better visibility

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -35,7 +35,7 @@ const Sidebar = () => {
          {/* <div className='flex w-full'> */}
         <button 
           onClick={toggleSidebar} 
-          className=" top-3 left-4 z-20 text-white bg-gray-600 p-2 m-3  rounded-md"
+          className=" top-3 left-4 z-20 text-white bg-gray-800 p-2 m-3  rounded-md"
         >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
