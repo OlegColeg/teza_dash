e127208766b4dd5ef4661e087d33819bdfb60a05
OlegColeg <ooale47@gmail.com>
2025-06-20 11:54:40 +0300

    fix: correct head section indentation in RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+        <head>
+      <title>Dashboard Oală Oală</title>
+    </head>
       <body>
         <div className="flex">
           {/* Sidebar Component */}
